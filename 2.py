e4a4b103b8429edf36a80810c7f9349020967bbf
V587KAY <40952883+V587KAY@users.noreply.github.com>
2018-07-09 16:35:30 +0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,7 @@
+print('please enter your age')
+age = input()
+age = int(age)
+if age >= 18:
+    print('dajiji')
+else:
+    print('xiaojiji')
---
+++ 3.py
@@ -0,0 +1,8 @@
+n = 123
+f = 456.789
+s1 = 'hello,world''\n'
+s2 = 'Hello,\'Mary\'''\n'
+s3 = r'Hello,"Baby"''\n'
+s4 = r'''Hello,
+Lisa!!!'''
+print(n, f, s1, s2, s3, s4)
